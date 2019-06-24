e9c407585a411b2e42370a18aa35ff4ec54df976
Abu Bákr Siddique <siddiqiabubaker079@gmail.com>
2019-06-24 03:59:45 -0700

    Add files via upload

---
+++ search.php
@@ -0,0 +1,63 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($con, "search");
+
+$result = "";
+
+if (isset($_POST["sub"]))
+	{
+		$search = $_POST["text"];
+
+		$sql = "SELECT * FROM wizard WHERE FirstName LIKE '%$search%' OR LastName LIKE '%$search%'";
+		$query = mysqli_query($con, $sql);
+
+		$rows = mysqli_num_rows($query);
+
+		if ($rows == 0)
+		{
+			$result = "Here is no result about the search";
+			print($result);
+		}
+
+		else 
+		{
+			while ($info = mysqli_fetch_array($query))
+			{
+				$fstN = $info["FirstName"];
+				$lstN = $info["LastName"];
+				$id = $info["ID"];
+
+				$result = "" .$fstN. " " .$lstN. "<br>" . "";
+				print($result);
+
+
+			}
+		}
+
+
+
+
+	}
+
+?>
+
+<!doctype html>
+<html lang="en-IN">
+<head>
+
+	<title>Search</title>
+
+</head>
+
+<body>
+
+<form action="search.php"method="POST">
+
+	<input type="text"placeholder="Search here..."name="text">
+	<input type="submit"name="sub"value="Submit">
+</form>
+
+
+</body>
+</html>
