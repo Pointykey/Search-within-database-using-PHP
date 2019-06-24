b11e62f589296438a03f78e3229797b6dcadf2d4
Abu Bákr Siddique <siddiqiabubaker079@gmail.com>
2019-06-24 04:00:35 -0700

    Add files via upload

---
+++ search.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2019 at 06:25 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `search`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wizard`
+--
+
+CREATE TABLE `wizard` (
+  `ID` int(11) NOT NULL,
+  `FirstName` text NOT NULL,
+  `LastName` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `wizard`
+--
+
+INSERT INTO `wizard` (`ID`, `FirstName`, `LastName`) VALUES
+(1, 'Abu Bakr', 'Siddique'),
+(2, 'Sahe', 'Ban'),
+(3, 'Rishu', 'Ranjan'),
+(5, 'Kamran', 'Mohsin'),
+(6, 'Noor', 'Imam');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `wizard`
+--
+ALTER TABLE `wizard`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `wizard`
+--
+ALTER TABLE `wizard`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
